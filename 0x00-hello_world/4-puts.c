98390a9e57f032b407c743df6f99966bc47de333
emmylash22 <akinnimiemmanuel@gmail.com>
2022-06-29 16:52:49 -0700

    puts_new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  * main function - puts programming is like a
- * multilingual puzzle
- * */
+ * multilingual puzzle* */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
