538c457b56e34a4d9fc15a2863dafaa44ea23e4f
emmylash22 <akinnimiemmanuel@gmail.com>
2022-08-11 18:05:38 -0700

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,8 +24,3 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 #endif
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
